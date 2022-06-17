be9a405572702e243b657ff6eb2f110b74655b74
bsstibich <bsstibich@gmail.com>
2022-06-17 17:22:49 -0500

    tidying up code a smidge

--- main.py
+++ main.py
@@ -2,13 +2,13 @@ from phevaluator import evaluate_cards, Card
 from hole_class import Hole
 import itertools
 #create deck
-deck = [Card('As'), Card('Ac'), Card('Ad'), Card('Ah'), Card('2s'), Card('2c'), Card('2d'), Card('2h'),Card('3s'), Card('3c'), Card('3d'),
-        Card('3h'),Card('4s'), Card('4c'),
-        Card('4d'), Card('4h'),Card('5s'), Card('5c'), Card('5d'), Card('5h'),
-        Card('6s'), Card('6c'), Card('6d'), Card('6h'),Card('7s'), Card('7c'), Card('7d'),
-        Card('7h'),Card('8s'), Card('8c'), Card('8d'), Card('8h'),Card('9s'), Card('9c'), Card('9d'), Card('9h'),Card('Ts'), Card('Tc'),
-        Card('Td'), Card('Th'),Card('Js'), Card('Jc'), Card('Jd'), Card('Jh'),
-        Card('Qs'), Card('Qc'), Card('Qd'), Card('Qh'),Card('Ks'), Card('Kc'), Card('Kd'), Card('Kh')]
+deck = [Card('As'), Card('Ac'), Card('Ad'), Card('Ah'), Card('2s'), Card('2c'), Card('2d'), Card('2h'),
+        Card('3s'), Card('3c'), Card('3d'),Card('3h'),Card('4s'), Card('4c'),Card('4d'), Card('4h'),
+        Card('5s'), Card('5c'), Card('5d'), Card('5h'),Card('6s'), Card('6c'), Card('6d'), Card('6h'),
+        Card('7s'), Card('7c'), Card('7d'),Card('7h'),Card('8s'), Card('8c'), Card('8d'), Card('8h'),
+        Card('9s'), Card('9c'), Card('9d'), Card('9h'),Card('Ts'), Card('Tc'),Card('Td'), Card('Th'),
+        Card('Js'), Card('Jc'), Card('Jd'), Card('Jh'),Card('Qs'), Card('Qc'), Card('Qd'), Card('Qh'),
+        Card('Ks'), Card('Kc'), Card('Kd'), Card('Kh')]
 
 
 running = 1
